5eae37f0a7304b8a2322353bd23d0005e0d091bd
fernando <fernandoaqs@outlook.com.br>
2025-05-27 22:38:48 -0300

    update theme installation to use WhiteSur GTK theme and change wallpaper reference

--- customization/scripts/theme.sh
+++ customization/scripts/theme.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-install_gtk_theme_tokyo_night(){
-	git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme.git
-
-	./Tokyonight-GTK-Theme/themes/install.sh --libadwaita -t purple -c dark -s compact --tweaks black
-	sudo rm -rf Tokyonight-GTK-Theme
-
-	sudo cp -r $HOME/.themes/* /usr/share/themes
-
-	gsettings set org.gnome.desktop.interface gtk-theme "Tokyonight-Purple-Dark-Compact"
-	gsettings set org.gnome.desktop.wm.preferences theme "Tokyonight-Purple-Dark-Compact"
-
-	# Setting theme icons to flatpak apps
-	sudo flatpak override --filesystem=$HOME/.themes
-	sudo flatpak override --filesystem=$HOME/.icons
-	flatpak override --user --filesystem=xdg-config/gtk-4.0
-	sudo flatpak override --filesystem=xdg-config/gtk-4.0
-}
-
-
 install_gtk_theme(){
-	git clone https://github.com/vinceliuice/Colloid-gtk-theme.git
+	git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 
-	./Colloid-gtk-theme/install.sh --tweaks float --libadwaita -s compact
-	sudo rm -rf Colloid-gtk-theme
+	./WhiteSur-gtk-theme/install.sh -c dark -o solid
+	sudo rm -rf WhiteSur-gtk-theme
 
 	sudo cp -r $HOME/.themes/* /usr/share/themes
 
-	gsettings set org.gnome.desktop.interface gtk-theme "Colloid-Dark-Compact"
-	gsettings set org.gnome.desktop.wm.preferences theme "Colloid-Dark-Compact"
+	gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-Dark-solid"
+	gsettings set org.gnome.desktop.wm.preferences theme "WhiteSur-Dark-solid"
 
 	# Setting theme icons to flatpak apps
 	sudo flatpak override --filesystem=$HOME/.themes
@@ -39,9 +20,10 @@ install_gtk_theme(){
 
 change_wallpaper(){
 	sudo cp -r ./.dotfiles/wallpapers /usr/share/backgrounds/
-	gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpapers/58.jpg
-	gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/wallpapers/58.jpg
+	gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/wallpapers/124.jpg
+	gsettings set org.gnome.desktop.background picture-uri-dark file:///usr/share/backgrounds/wallpapers/124.jpg
 }
 
 export change_wallpaper
 export install_gtk_theme
+Compact
