555f2904819bf7480a726916f695091c8e3ff12c
fernando <fernandoaqs@outlook.com.br>
2025-05-27 22:39:21 -0300

    refactor icon installation script: update icon theme source and clean up code

--- customization/scripts/icon.sh
+++ customization/scripts/icon.sh
@@ -4,21 +4,21 @@ install_cursor(){
 	wget https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz
 	tar -xvf Bibata-Modern-Ice.tar.xz
 	rm Bibata-Modern-Ice.tar.xz
-	
+
 	sudo mv Bibata-* /usr/share/icons/
-	
+
 	gsettings set  org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
 }
 
 install_icons(){
-	git clone https://github.com/vinceliuice/Colloid-icon-theme.git
-	./Colloid-icon-theme/install.sh -s catppuccin -t purple
-	
+	git clone https://github.com/vinceliuice/Tela-icon-theme.git
+	./Tela-icon-theme/install.sh
+
 	sudo cp -r $HOME/.local/share/icons/* /usr/share/icons
 	sudo cp -r $HOME/.local/share/icons/* $HOME/.icons
-	rm -rf Colloid-icon-theme
-	
-	gsettings set  org.gnome.desktop.interface icon-theme 'Colloid-Purple-Catppuccin-Dark'
+	rm -rf Tela-icon-theme
+
+	gsettings set  org.gnome.desktop.interface icon-theme 'Tela-dark'
 }
 
 export install_icons
